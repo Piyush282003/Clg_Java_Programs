bcdb410fdec749b7653a91df5dab4cd3847cf81e
Piyush <palpiyush2003@gmail.com>
2023-05-09 01:53:02 +0530

    new File

--- src/myclg/J49_upper_lower_Case.java
+++ src/myclg/J49_upper_lower_Case.java
@@ -1,18 +1,40 @@
 package myclg;
 
+import java.util.Scanner;
+
 public class J49_upper_lower_Case {
     public static void main(String[] args) {
-        String s="abcdef";
-        String x="";
-        for(int i=0;i<s.length();i++){
-//            if(Character.isLowerCase(s.charAt(i))){
-            if(i%2==0){
-                x+=Character.toUpperCase(s.charAt(i));
-            }
-            else{
-                x+=s.charAt(i);
+//        String s="abcdef";
+//        String x="";
+//        for(int i=0;i<s.length();i++){
+////            if(Character.isLowerCase(s.charAt(i))){
+//            if(i%2==0){
+//                x+=Character.toUpperCase(s.charAt(i));
+//            }
+//            else{
+//                x+=s.charAt(i);
+//            }
+//        }
+//        System.out.println(x);
+
+
+        Scanner obj = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String s1 = obj.nextLine();
+
+        char[] chArr = s1.toCharArray();
+
+        for (int i = 0; i < chArr.length; i++) {
+            if (i % 2 != 0) {
+                chArr[i] = Character.toUpperCase(chArr[i]);
+            } else {
+                chArr[i] = Character.toLowerCase(chArr[i]);
             }
         }
-        System.out.println(x);
+
+        String s2 = new String(chArr);
+
+        System.out.println("New String = " + s2);
     }
 }
